478f390b53f34cf0350728c9a86be552fef473d3
Ahmed Almabhouh <az540546@gmail.com>
2025-05-14 13:37:30 +0300

    Install Panels

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@hotel.com',
+            'password' => Hash::make('password'),
+            'is_admin' => true,
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Regular User',
+            'email' => 'user@hotel.com',
+            'password' => Hash::make('password'),
+            'is_admin' => false,
         ]);
     }
 }
